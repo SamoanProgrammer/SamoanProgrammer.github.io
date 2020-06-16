ee4c5e22c3d1edad4554bcec860fd2836ef1bfe8
Canon Taito <canon@companionsoftware.com.au>
2020-06-16 20:49:00 +1000

    Algorithm Fibonnaci and Prime Numbers

--- FCC_algorithms.js
+++ FCC_algorithms.js
@@ -227,3 +227,52 @@ function uniteUnique(...arr) {
 }
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+/*Sum All Odd Fibonacci Numbers
+Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+The first two numbers in the Fibonacci sequence are 1 and 1. Every additional number in the sequence is the sum of the two previous numbers. 
+The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.*/
+function sumFibs(num) {
+  let fibArr = [1,1];
+  let retNum = 0;
+  for (let i = 1; fibArr[i] < num; i++)
+  {
+    let sum = fibArr[i-1] + fibArr[i];
+    fibArr.push(sum);
+    if (sum == 2 || (sum%2 != 0 && sum <= num))
+    {
+      retNum += sum;
+      console.log(`${sum} Running Total: ${retNum}`);
+    }
+  }
+  return retNum;
+}
+
+sumFibs(75025);
+
+/*Sum All Primes
+A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. 
+For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.*/
+function IsPrime (num) {
+  for (let i = 2; i <= num; i++)
+  {
+    if (num != i && num%i == 0)
+      return false;
+  }
+  return true;
+}
+
+function sumPrimes(num) {
+  let retNum = 0;
+  for (let i = 2; i <= num; i++)
+  {
+    if (IsPrime(i))
+      retNum += i;
+  }
+  return retNum;
+}
+
+console.log(sumPrimes(977)); // should return 73156.
+
