49b615ee66d4fa5b44f6354732d885f4a54cbad6
Canon Taito <canon@companionsoftware.com.au>
2020-06-19 21:46:27 +1000

    Algorithm Arguments

--- FCC_algorithms.js
+++ FCC_algorithms.js
@@ -385,3 +385,41 @@ function truthCheck(collection, pre) {
 }
 
 truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age");
+
+/*Arguments Optional
+Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
+Calling this returned function with a single argument will then return the sum:
+var sumTwoAnd = addTogether(2);
+sumTwoAnd(3) returns 5.
+If either argument isn't a valid number, return undefined.*/
+function addTogether(...args) {
+  if (args.length == 2)
+  {
+    let val1 = args[0];
+    let val2 = args[1];
+    
+    if (typeof(val1) != "number" || typeof(val2) != "number")
+      return;
+    
+    return val1 + val2;
+  }
+  else if (args.length == 1)
+  {
+    if (typeof(args[0]) != "number")
+      return;
+
+    return function sumTwo(y) {
+      if (typeof(y)!= "number")
+        return;
+
+      return args[0] + y;
+    }
+  }
+  return false;
+}
+
+addTogether(2)([3]);
+addTogether(2,3);
+addTogether(5)(7);
+addTogether(2, "3")
